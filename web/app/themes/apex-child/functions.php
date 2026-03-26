e07d6651564e9fbee02378613062f2badd81af64
ronyn0 <ninjalizard7@gmail.com>
2026-03-26 17:12:02 -0400

    removed wordpress howdy

--- web/app/themes/apex-child/functions.php
+++ web/app/themes/apex-child/functions.php
@@ -4,10 +4,10 @@ add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('apex-child', get_stylesheet_uri(), ['astra-parent']);
 });
 
-add_filter('admin_bar_menu', function($wp_admin_bar) {
-    $user = wp_get_current_user();
-    $wp_admin_bar->add_node([
-        'id'    => 'my-account',
-        'title' => $user->display_name,
-    ]);
-}, 25);
+add_filter('gettext', 'change_howdy', 10, 3);
+function change_howdy($translated, $original, $domain) {
+    if ($original === 'Howdy, %s') {
+        return '%s';
+    }
+    return $translated;
+}
